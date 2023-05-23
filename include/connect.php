ae8bccbc10f7eebcd68d0dec186def3aa9ba3810
Shangwe Nyota <shangwemnyota@gmail.com>
2023-05-23 09:40:26 -0500

    Integrated PHP + Database

---
+++ include/connect.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Hey ";
+    DEFINE('DB_HOSTNAME', 'localhost');
+    DEFINE('DB_DATABASE', 'personal_site');
+    DEFINE('DB_USERNAME', 'root');
+    DEFINE('DB_PASSWORD', 'root');
+    echo "you!";
+    include("db_query.php");
