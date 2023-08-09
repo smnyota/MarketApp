220a06c5d1ab506ac27afdff6987661fe0a9a9c9
Shangwe Nyota <shangwemnyota@gmail.com>
2023-08-09 14:27:30 -0500

    Updating Documentation

--- awsEndpoint.php
+++ awsEndpoint.php
@@ -1,5 +1,4 @@
 <?php
-
 require 's3/vendor/autoload.php';
 
 use Aws\S3\S3Client;
@@ -11,9 +10,9 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
 $dotenv->load();
 
 function movetoAWS($uploadedFile) {
-	// AWS Info
+
 $bucketName = 'washulist';
-// Connect to AWS
+
 try {
 	$s3 = S3Client::factory(
 		array(
@@ -26,18 +25,13 @@ try {
 		)
 	);
 } catch (Exception $e) {
-	// We use a die, so if this fails. It stops here. Typically this is a REST call so this would
-	// return a json object.
+	
 	die("Error: " . $e->getMessage());
 }
-
-
-// For this, I would generate a unqiue random string for the key name. But you can do whatever.
 
 $keyName = 'photos/'. basename($uploadedFile["file"]['name']); 	//sets the name in S3 aws
 $pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
 
-// Add it to S3
 try {
 	// Uploaded:
 	$file = $uploadedFile["file"]['tmp_name'];
@@ -57,8 +51,6 @@ try {
 	die('Error:' . $e->getMessage());
 }
 
-// $accessCode = "123"; //randomize this
-// $accessCode = uniqid();
 return $keyName;
 
 
