309352fcc27eb6a3845e715502a21d91c59c7950
Shangwe Nyota <shangwemnyota@gmail.com>
2023-08-10 10:48:04 -0500

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -25,6 +25,7 @@
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/cert.pem",
     );
     $pdo = new PDO($dsn, $DbUser, $DbPassword, $opt);
 
