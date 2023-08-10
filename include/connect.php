4b456e3e990db90785e5b936ed4485aa224f7b32
Shangwe Nyota <shangwemnyota@gmail.com>
2023-08-10 10:57:28 -0500

    add crt file instead of pem

--- include/connect.php
+++ include/connect.php
@@ -25,7 +25,7 @@
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
-        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/cert.pem",
+        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
     );
     $pdo = new PDO($dsn, $DbUser, $DbPassword, $opt);
 
